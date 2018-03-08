1bcd653e11884ed4e32187b3a1dfa4d3328eafac
Bilash, Ryan <r.bilash112233@gmail.com>
2018-03-08 17:53:01 -0500

    Commit

--- app/src/main/java/com/example/ryan/cbutton/MainActivity.java
+++ app/src/main/java/com/example/ryan/cbutton/MainActivity.java
@@ -7,20 +7,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
-    String buttonColor="red";
-    String voice="noVoice";
-    File file = new File("settings.txt");
+    public static File file = new File("settings.txt");
+    public static String getFile(int a){
+        try{
+            Scanner sc = new Scanner(new FileReader(file));
+            String s;
+            if(a==1){
+                s=sc.nextLine();
+                sc.close();
+                return s;
+            }else{
+                sc.nextLine();
+                s=sc.nextLine();
+                return s;
+            }
+        }catch(Exception e){}
+        if(a==1){
+            return "red";
+        }else{
+            return "Sound";
+        }
+    }
+
+
+    static String buttonColor=getFile(1);
+    static String voice=getFile(2);
+
 
     public static int time = 0;
     public static void changeTime(int i){
@@ -33,22 +57,25 @@ public class MainActivity extends AppCompatActivity {
     public void changeButtonColor(String n){//have the thing be a spinner with the different colors listed
         buttonColor=n;
     }
-    public void Voice(boolean n){
+    public void Sound(boolean n){
         if(n){
-            voice="voice";
+            voice="Sound";
         }else{
-            voice="noVoice";
+            voice="noSound";
         }
     }
-    public boolean voiceEnabled(){
-        if(voice.equals("voice")){
+    public boolean soundEnabled(){
+        if(voice.equals("Sound")){
             return true;
         }else{
             return false;
         }
     }
-    public String getBC(){
+    public static String getBC(){
         return buttonColor;
+    }
+    public static String getS(){
+        return voice;
     }
 
     public MediaPlayer bgm;
@@ -65,10 +92,12 @@ public class MainActivity extends AppCompatActivity {
 
         bgmE();
 
-
         bgm.seekTo(getTime());
         bgm.start();
         bgm.setLooping(true);
+
+
+
         final MediaPlayer click = MediaPlayer.create(this, R.raw.click);
 
         final ArrayAdapter<String> compList = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.comps));
@@ -80,23 +109,38 @@ public class MainActivity extends AppCompatActivity {
             BufferedReader br=new BufferedReader(new FileReader(file));
             buttonColor=br.readLine();
             voice=br.readLine();
-        } catch (FileNotFoundException e) {
-            fileFound=false;
-        } catch (IOException e) {
+        } catch (Exception e) {
             fileFound=false;
         }
         if(!fileFound){
             try {
                 FileWriter fw=new FileWriter(file);
-                fw.append("red\nnoVoice");
+                fw.append("red\nSound");
             } catch (IOException e) {
             }
         }else{
 
         }
 
-        Button button =(Button)findViewById(R.id.button);
+        if(!soundEnabled()){
+            bgm.stop();
+        }else{
+            bgm.start();
+        }
+
+        ImageButton button =(ImageButton)findViewById(R.id.button);
         //button.setImageResource()
+
+        if(buttonColor.equals("red")){
+            button.setImageResource(R.drawable.red);
+        }else if(buttonColor.equals("purple")){
+            button.setImageResource(R.drawable.purple);
+        }else if(buttonColor.equals("blue")){
+            button.setImageResource(R.drawable.blue);
+        }else if(buttonColor.equals("green")){
+            button.setImageResource(R.drawable.green);
+        }
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -130,6 +174,7 @@ public class MainActivity extends AppCompatActivity {
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                click.start();
                 startActivity(new Intent(MainActivity.this, Settings.class));
             }
         });
--- app/src/main/java/com/example/ryan/cbutton/Settings.java
+++ app/src/main/java/com/example/ryan/cbutton/Settings.java
@@ -3,12 +3,44 @@ package com.example.ryan.cbutton;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 import static com.example.ryan.cbutton.MainActivity.changeTime;
+import static com.example.ryan.cbutton.MainActivity.getBC;
+import static com.example.ryan.cbutton.MainActivity.getS;
 import static com.example.ryan.cbutton.MainActivity.getTime;
 
 public class Settings extends AppCompatActivity {
     public MediaPlayer bgm;
+
+    String buttonColor=getBC();
+    String voice = getS();
+    File file = new File("settings.txt");
+
+    public BufferedWriter outStream;
+
+    public boolean soundEnabled(){
+        if(voice.equals("Sound")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void Sound(boolean n){
+        if(n){
+            voice="Sound";
+        }else{
+            voice="noSound";
+        }
+    }
 
     public void bgmE(){
         bgm = MediaPlayer.create(this, R.raw.bgm);
@@ -20,12 +52,118 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        bgmE();
+        final MediaPlayer click = MediaPlayer.create(this, R.raw.click);
 
+        bgmE();
 
         bgm.seekTo(getTime());
         bgm.start();
         bgm.setLooping(true);
+
+        if(!soundEnabled()){
+            bgm.stop();
+        }else{
+            bgm.start();
+        }
+
+
+
+        Switch sw = (Switch)findViewById(R.id.switch1);
+        sw.setChecked(soundEnabled());
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(soundEnabled()){
+                    bgm.stop();
+                    click.start();
+                }else{
+                    bgm.start();
+                }
+                Sound(!soundEnabled());
+                try {
+                    outStream = new BufferedWriter(new FileWriter("settings.txt"));
+                    outStream.newLine();
+                    outStream.write(buttonColor);
+                    outStream.newLine();
+                    outStream.write(voice);
+                    outStream.close();
+                } catch (Exception e) {}
+
+            }
+        });
+        ImageButton red = (ImageButton)findViewById(R.id.ButtonR);
+        ImageButton blu = (ImageButton)findViewById(R.id.ButtonB);
+        ImageButton pur = (ImageButton)findViewById(R.id.ButtonP);
+        ImageButton gre = (ImageButton)findViewById(R.id.ButtonG);
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonColor = "red";
+                if(soundEnabled()){
+                    click.start();
+                }
+                try {
+                    outStream = new BufferedWriter(new FileWriter("settings.txt"));
+                    outStream.newLine();
+                    outStream.write(buttonColor);
+                    outStream.newLine();
+                    outStream.write(voice);
+                    outStream.close();
+                } catch (Exception e) {}
+            }
+        });
+        blu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonColor = "blue";
+                if(soundEnabled()){
+                    click.start();
+                }
+                try {
+                    outStream = new BufferedWriter(new FileWriter("settings.txt"));
+                    outStream.newLine();
+                    outStream.write(buttonColor);
+                    outStream.newLine();
+                    outStream.write(voice);
+                    outStream.close();
+                } catch (Exception e) {}
+            }
+        });
+        gre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonColor = "green";
+                if(soundEnabled()){
+                    click.start();
+                }
+                try {
+                    outStream = new BufferedWriter(new FileWriter("settings.txt"));
+                    outStream.newLine();
+                    outStream.write(buttonColor);
+                    outStream.newLine();
+                    outStream.write(voice);
+                    outStream.close();
+                } catch (Exception e) {}
+            }
+        });
+        pur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonColor = "purple";
+                if(soundEnabled()){
+                    click.start();
+                }
+                try {
+                    outStream = new BufferedWriter(new FileWriter("settings.txt"));
+                    outStream.newLine();
+                    outStream.write(buttonColor);
+                    outStream.newLine();
+                    outStream.write(voice);
+                    outStream.close();
+                } catch (Exception e) {}
+            }
+        });
 
     }
     protected void onPause(){
